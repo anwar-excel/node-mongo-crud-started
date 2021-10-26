613e208c985f03f26e15b5d8bd6791bc804efc3a
unknown <anwarhosen575@gmail.com>
2021-10-26 08:38:12 +0600

    first commit

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const AddUser = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const handleAddUser = e => {
+        e.preventDefault();
+    }
     return (
         <div>
             <h2>This is Add User</h2>
+            <form onSubmit={ } >
+                <input type="time" name="" id="" ref={nameRef} />
+                <input type="email" name="" id="" ref={emailRef} />
+                <input type="submit" value="Add" />
+            </form>
         </div>
     );
 };
